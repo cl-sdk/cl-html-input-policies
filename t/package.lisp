98284279c59294e6630330b77150f8260a84b7b5
Bruno Dias <dias.h.bruno@gmail.com>
2026-05-10 10:27:59 -0300

    Apply suggestions from code review
    Co-authored-by: Bruno Dias <dias.h.bruno@gmail.com>

--- t/package.lisp
+++ t/package.lisp
@@ -1,11 +1,7 @@
 (defpackage #:cl-html-input-policies-test
-  (:use #:cl #:fiveam #:cl-html-input-policies)
-  (:export #:run-tests))
+  (:use #:cl #:fiveam #:cl-html-input-policies))
 
 (in-package #:cl-html-input-policies-test)
 
 (def-suite html-input-policies-suite)
 (in-suite html-input-policies-suite)
-
-(defun run-tests ()
-  (run! 'html-input-policies-suite))
